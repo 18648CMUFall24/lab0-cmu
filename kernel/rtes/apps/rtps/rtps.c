0217b0e15cbda37e690a722815147e3d2d57059a
Yuhong <yuhongy@andrew.cmu.edu>
2024-10-05 01:12:36 -0400

    add error handling and application is working but need refine

--- kernel/rtes/apps/rtps/rtps.c
+++ kernel/rtes/apps/rtps/rtps.c
@@ -20,6 +20,7 @@
 #include <sys/types.h>
 #include <sys/syscall.h>
 #include <linux/termios.h>
+#include <signal.h>
 
 #define SYS_COUNT 377 // count_rt_threads
 #define SYS_LIST 378 // list_rt_threads
@@ -32,7 +33,7 @@ struct rt_thread
     pid_t tid;    /* Thread ID */
     pid_t pid;    /* Process ID */
     int priority; /* Thread Priority */
-    char *name;   /* Name (command) */
+    char name[20];   /* Name (command) */
 };
 
 void get_terminal_size(int *rows, int *cols)
@@ -45,14 +46,25 @@ void get_terminal_size(int *rows, int *cols)
 
 void print_threads(struct rt_thread* list, int loop_len)
 {
+    printf("HERE: 2\n");
+
     int i = 0;
     // Dummy data for process display (replace with actual process data)
     printf("TID   PID   PRIORITY     COMMAND\n");
     for (i = 0; i < loop_len; i++)
     {
+        // printf("%6d  %6d   %4d   %s\n",
+        //         list[i]->tid, list[i]->pid, list[i]->priority, list[i]->name);
         printf("%6d  %6d   %4d   %s\n",
                 list[i].tid, list[i].pid, list[i].priority, list[i].name);
     }
+}
+
+void handle_sigint(int sig)
+{
+    // Show the cursor again when the program ends
+    printf("\033[?25h");
+    exit(0);
 }
 
 int main()
@@ -61,6 +73,7 @@ int main()
     int refresh_rate = 2; // refresh rate in 2 seconds
     struct rt_thread* rt_threads_list;
 
+    signal(SIGINT, handle_sigint);
     // Hide the cursor to make the display cleaner
     // printf("\033[?25l");
 
@@ -78,16 +91,36 @@ int main()
         }
         printf("Number of real-time threads: %d\n", count);
         
-        num_to_disp = MIN(rows, count);
+        // num_to_disp = MIN(rows, count);
+        num_to_disp = count;
+        if (num_to_disp <= 0)
+        {
+            printf("Error: Terminal size too small to display any threads\n");
+            sleep(refresh_rate);
+            continue;
+        }
+
         // Allocate list based on num to disp
         rt_threads_list = malloc(num_to_disp * sizeof(struct rt_thread));
+        if (!rt_threads_list) {
+            perror("Error: Unable to allocate memory for real-time thread list\n");
+            break;
+        }
+
         // Call list of threads
         if (syscall(SYS_LIST, rt_threads_list, MAX_THREADS) < 0){
-            printf("sys_list failed\n");
+            perror("Error: sys_list failed\n");
+            free(rt_threads_list);
             return -1;
         }
+
+        printf("HERE: 1\n");
+        printf("rt_threads_list=%p\n", rt_threads_list);
+        printf("OR rt_threads_list=%p\n", &rt_threads_list);
         // Print the threads
         print_threads(rt_threads_list, num_to_disp); 
+        printf("HERE: 10\n");
+
         // Free up malloc
         free(rt_threads_list);
 
