0712f21d674ce7b3c29cc3d840d368f585487101
Yuhong <yuhongy@andrew.cmu.edu>
2024-10-06 04:34:51 -0400

    add rounding and refactor the calc sys call

--- kernel/rtes/kernel/calc.c
+++ kernel/rtes/kernel/calc.c
@@ -20,37 +20,43 @@
 #include <linux/string.h>
 #include <linux/errno.h>
 #include <linux/syscalls.h>
+#include <linux/math64.h>
 
 
 #define BUFFER_SIZE 32
 // Frac bits: 12 (works for */ but errors in +-), 13 (same), 16 (works for +- but not /*)
-#define FRACTION_BITS 11
+#define FRACTION_BITS 20
 #define FRACTION_SCALE (1 << FRACTION_BITS)
 
-typedef long fixed_point_t;
+typedef int64_t fixed_point_t;
 
 /**
  * Convert a string to a fixed-point number
  */
-fixed_point_t str_to_fixed_point(const char *str)
-{
-    long int_part = 0;
-    long frac_part = 0;
-    int is_neg = 0;
+fixed_point_t str_to_fixed_point(const char *str) {
+    int64_t int_part = 0;       //integer part for the fixed point number
+    int64_t frac_part = 0;      //fractional part for the fixed point number
+    uint8_t is_neg = 0;         // flag to check if the number is negative even though we don't input negative num
     int i = 0;
-    long multiplier = FRACTION_SCALE / 10;
+    int multiplier = FRACTION_SCALE / 10;   // parsing the frac part as an integer divide by 10
     fixed_point_t fixed_point;
+    int decimal_places = 0;
 
     // Check if number is negative
     if (str[0] == '-')
     {
         is_neg = 1;
         i++;
+    } else if (str[0] == '+') {
+        i++;    // Skip the + sign
     }
 
     // Parse integer part
-    while (str[i] != '.' && str[i] != '\0')
-    {
+    while (str[i] != '.' && str[i] != '\0') {
+        if (str[i] < '0' || str[i] > '9') {
+            return -EINVAL; // Invalid character
+        }
+
         int_part = int_part * 10 + (str[i] - '0');
         i++;
     }
@@ -59,15 +65,40 @@ fixed_point_t str_to_fixed_point(const char *str)
     if (str[i] == '.')
     {
         i++; // Move after the .
-        while (str[i] != '\0' && multiplier > 0)
+        decimal_places = 0;
+        while (str[i] != '\0' && multiplier > 0 && decimal_places < 3)
         {
             frac_part += (str[i] - '0') * multiplier;
             multiplier /= 10;
             i++;
+            decimal_places++;
         }
     }
     fixed_point = (int_part << FRACTION_BITS) + frac_part;
     return is_neg ? -fixed_point : fixed_point;
+}
+
+/**
+ * Convert a fixed-point number to a string
+ */
+void fixed_point_to_str(fixed_point_t fixed_num, char *result, size_t buf_size) {
+    int64_t int_part = fixed_num >> FRACTION_BITS;  // integer part (cast to int)
+    int64_t frac_part = fixed_num & (FRACTION_SCALE - 1); // fractional part (mask out integer part)
+    frac_part = frac_part * 1000 + FRACTION_SCALE / 2; // Round to 3 decimal places
+    do_div(frac_part, FRACTION_SCALE);
+
+    if (frac_part == 1000) {
+        int_part++;
+        frac_part = 0;
+    }
+
+    // If the fractional part is zero, print only the integer part
+    if (frac_part == 0) {
+        snprintf(result, buf_size, "%lld", int_part);  // No decimal part
+    } else {
+        // Format the number with up to 3 decimal places
+        snprintf(result, buf_size, "%lld.%03lld", int_part, frac_part);
+    }
 }
 
 /**
@@ -94,8 +125,7 @@ fixed_point_t str_to_fixed_point(const char *str)
  */
 SYSCALL_DEFINE4 (calc, const char*, param1, const char*, param2, char, operation, char*, result ) {
     fixed_point_t num1, num2, res;
-    long int_part, frac_part;
-    int is_neg;
+    int is_neg = 0;
 
     // Check both parameters are valid
     if (param1 == NULL || param2 == NULL || result == NULL)
@@ -103,56 +133,42 @@ SYSCALL_DEFINE4 (calc, const char*, param1, const char*, param2, char, operation
     // Check param is non-negative rational number
     if (param1[0] == '-' || param2[0] == '-')
         return -EINVAL;
-    // Check operation is valid
-    if (operation != '-' && operation != '+' && operation != '*' && operation != '/')
-        return -EINVAL;
 
     // Convert param1 and param2 to float
     num1 = str_to_fixed_point(param1);
     num2 = str_to_fixed_point(param2);
 
-
     // Perform operation
-    if (operation == '-')
-    {
-        res = num1 - num2;
-    }
-    else if (operation == '+')
-    {
+    switch (operation) {
+    case '+':
         res = num1 + num2;
-    }
-    else if (operation == '*')
-    {
+        break;
+    case '-':
+        res = num1 - num2;
+        break;
+    case '*':
         res = (num1 * num2) >> FRACTION_BITS;
-    }
-    else if (operation == '/')
-    {
-        if (num2 == 0)
-            return -1;
-        res = (num1 << FRACTION_BITS) / num2;
-    }
-    else
-    {
-        return -1;
+        break;
+    case '/':
+        if (num2 == 0) return -EINVAL; // Division by zero
+
+        res = div64_s64((num1<<FRACTION_BITS), num2);
+        break;
+    default:
+        return -EINVAL; // Invalid operation
     }
 
     // Write result to buffer
-    is_neg = res < 0;
-    if (is_neg)
+    if (res < 0) {
+        is_neg = 1;
         res = -res;
-
-    int_part = res >> FRACTION_BITS;
-    frac_part = ((res & (FRACTION_SCALE - 1)) * 1000L) >> FRACTION_BITS;
-    if (frac_part != 0){
-        // Remove trailing zeros
-        while (frac_part % 10 == 0) {
-            frac_part /= 10;
-        }
     }
 
-    if (snprintf(result, BUFFER_SIZE, "%s%ld.%ld", is_neg ? "-" : "", int_part, frac_part) < 0)
-    {
-        return -1; // error
+    if (is_neg) {
+        snprintf(result, BUFFER_SIZE, "-");
+        fixed_point_to_str(res, result + 1, BUFFER_SIZE - 1);
+    } else {
+        fixed_point_to_str(res, result, BUFFER_SIZE);
     }
 
     return 0; // success
