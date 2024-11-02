1495614f3a6c2d3d5c591d53bf88bcdd6e75596a
Weichen Qiu <57570491+w3ichen@users.noreply.github.com>
2024-11-01 23:16:49 -0400

    modified taskmon.c to include tid files

--- kernel/rtes/modules/taskmon/taskmon.c
+++ kernel/rtes/modules/taskmon/taskmon.c
@@ -32,16 +32,17 @@ MODULE_LICENSE("GPL"); // Required to use some functions
 static bool taskmon_enabled = false;
 static struct kobject *rtes_kobj;    // kobject for /rtes
 static struct kobject *taskmon_kobj; // kobject for /rtes/taskmon
+static struct kobject *util_kobj;    // kobject for /rtes/taskmon/util
 
 // When the user reads the sysfs file
-static ssize_t taskmon_show(struct kobject *kobj, struct kobj_attribute *attr, char *buf)
+static ssize_t enabled_show(struct kobject *kobj, struct kobj_attribute *attr, char *buf)
 {
     // Return the current value of taskmon_enabled as 0 or 1
     return sprintf(buf, "%d\n", (int)taskmon_enabled);
 }
 
 // When the user writes to the sysfs file
-static ssize_t taskmon_store(struct kobject *kobj, struct kobj_attribute *attr, const char *buf, size_t count)
+static ssize_t enabled_store(struct kobject *kobj, struct kobj_attribute *attr, const char *buf, size_t count)
 {
     // Set taskmon_enabled to true or false and start or stop monitoring accordingly
     if (buf[0] == '1')
@@ -59,19 +60,44 @@ static ssize_t taskmon_store(struct kobject *kobj, struct kobj_attribute *attr, 
     return count;
 }
 
-/** Initializes the kobj_attribute struct with the taskmon_show and taskmon_store functions
+// When the user reads the sysfs file
+static ssize_t tid_show(struct kobject *kobj, struct kobj_attribute *attr, char *buf)
+{
+    // Return the current value of taskmon_enabled as 0 or 1
+    return sprintf(buf, "%d\n", (int)taskmon_enabled);
+}
+
+// When the user writes to the sysfs file
+static ssize_t tid_store(struct kobject *kobj, struct kobj_attribute *attr, const char *buf, size_t count)
+{
+    // Set taskmon_enabled to true or false and start or stop monitoring accordingly
+    if (buf[0] == '1')
+    {
+        taskmon_enabled = true;
+        // Start monitoring
+        printk(KERN_INFO "Taskmon enabled\n");
+    }
+    else if (buf[0] == '0')
+    {
+        taskmon_enabled = false;
+        // Stop monitoring
+        printk(KERN_INFO "Taskmon disabled\n");
+    }
+    return count;
+}
+
+/** Initializes the kobj_attribute struct with the enabled_show and enabled_store functions
  *   - enabled is the name of the sysfs file
  *   - 0660 is the permissions of the sysfs file, owner can read/write, group can read/write, others can't access
- *   - taskmon_show is the function to call when the user reads the sysfs file
- *   - taskmon_store is the function to call when the user writes to the sysfs file
+ *   - enabled_show is the function to call when the user reads the sysfs file
+ *   - enabled_store is the function to call when the user writes to the sysfs file
  */
-static struct kobj_attribute taskmon_attr = __ATTR(enabled, 0660, taskmon_show, taskmon_store);
+static struct kobj_attribute enabled_attr = __ATTR(enabled, 0660, enabled_show, enabled_store);
 
-// Init kernel module
-static int __init taskmon_init(void)
+// Create the sysfs file /sys/rtes/taskmon/enabled
+int create_enabled_file(void)
 {
     int ret;
-
     // Create a kobject named "rtes" under the kernel kobject
     rtes_kobj = kobject_create_and_add("rtes", NULL); // use NULL for /sys/ instead of kernel_kobj); which is /sys/kernel
     if (!rtes_kobj)
@@ -79,7 +105,6 @@ static int __init taskmon_init(void)
         printk(KERN_ERR "Failed to create kobject: rtes\n");
         return -ENOMEM; // Error NO MEMory
     }
-
     // Create a kobject named "taskmon" under the "rtes" kobject
     taskmon_kobj = kobject_create_and_add("taskmon", rtes_kobj);
     if (!taskmon_kobj)
@@ -88,18 +113,69 @@ static int __init taskmon_init(void)
         kobject_put(rtes_kobj);
         return -ENOMEM;
     }
-
     // Create a sysfs file named "enabled" under the "taskmon" kobject
-    ret = sysfs_create_file(taskmon_kobj, &taskmon_attr.attr);
+    ret = sysfs_create_file(taskmon_kobj, &enabled_attr.attr);
     if (ret)
     {
         printk(KERN_ERR "Failed to create file: /sys/rtes/taskmon/enabled\n");
-        kobject_put(taskmon_kobj);
         kobject_put(rtes_kobj);
+        kobject_put(taskmon_kobj);
         return -1;
     }
-
     printk(KERN_INFO "Created file: /sys/rtes/taskmon/enabled\n");
+    return 0; // Success
+}
+
+// Create /sys/rtes/taskmon/util/<tid>
+int create_tid_file(int tid)
+{
+    int ret;
+    // Create a kobject named "rtes" under the kernel kobject
+    rtes_kobj = kobject_create_and_add("rtes", NULL); // use NULL for /sys/ instead of kernel_kobj); which is /sys/kernel
+    if (!rtes_kobj)
+    {
+        printk(KERN_ERR "Failed to create kobject: rtes\n");
+        return -ENOMEM; // Error NO MEMory
+    }
+    // Create a kobject named "taskmon" under the "rtes" kobject
+    taskmon_kobj = kobject_create_and_add("taskmon", rtes_kobj);
+    if (!taskmon_kobj)
+    {
+        printk(KERN_ERR "Failed to create kobject: taskmon\n");
+        kobject_put(rtes_kobj); // Release the kobject
+        return -ENOMEM;
+    }
+    // Create a kobject named "util" under the "taskmon" kobject
+    util_kobj = kobject_create_and_add("util", taskmon_kobj);
+    if (!util_kobj)
+    {
+        printk(KERN_ERR "Failed to create kobject: util\n");
+        kobject_put(rtes_kobj);
+        kobject_put(taskmon_kobj);
+        return -ENOMEM;
+    }
+    // Create a sysfs file named "<tid>" under the "taskmon" kobject
+    static struct kobj_attribute tid_attr = __ATTR(tid, 0660, tid_show, tid_store);
+    ret = sysfs_create_file(util_kobj, &tid_attr.attr);
+    if (ret)
+    {
+        printk(KERN_ERR "Failed to create file: /sys/rtes/taskmon/util/<tid>\n");
+        kobject_put(rtes_kobj);
+        kobject_put(taskmon_kobj);
+        kobject_put(util_kobj);
+        return -1;
+    }
+    printk(KERN_INFO "Created file: /sys/rtes/taskmon/util/%d\n", tid);
+
+    return 0; // Success
+}
+
+// Init kernel module
+static int __init taskmon_init(void)
+{
+    if (create_enabled_file() != 0)
+        return -1;
+
     return 0;
 }
 
@@ -107,8 +183,9 @@ static int __init taskmon_init(void)
 static void __exit taskmon_exit(void)
 {
     // Release the kobjects
-    kobject_put(taskmon_kobj);
     kobject_put(rtes_kobj);
+    kobject_put(taskmon_kobj);
+    kobject_put(util_kobj);
 }
 
 module_init(taskmon_init);
